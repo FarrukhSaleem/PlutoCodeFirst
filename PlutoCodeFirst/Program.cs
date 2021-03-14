a6acdbd18bbe37e58d3dc4cbfd738bd23e541c79
Farrukh Saleem Sheikh <farrukhsaleem_01@yahoo.com>
2021-03-14 22:48:04 +0500

    Add project files.

---
+++ PlutoCodeFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoCodeFirst
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	public enum CourseLevel
+	{
+		Beginner = 1,
+		Intermediate = 2,
+		Advanced = 3
+	}
+}
